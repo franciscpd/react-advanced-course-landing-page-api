0f989196349191c54a2eab8988401f3bdb65ed81
Francisross Soares de Oliveira <franciscpd@gmail.com>
2020-07-23 15:05:09 -0300

    feat: initial setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '54764be4cf378634a1ee1a1f4c418432'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9158ee31-ae59-451b-a8f6-d8dbcd4d4ce7'
+};
